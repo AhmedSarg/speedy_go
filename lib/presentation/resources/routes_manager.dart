70a1a0a995b9b3fd1a65f1731a5ccbc6630ae620
AhmedSarg <91041775+AhmedSarg@users.noreply.github.com>
2024-04-13 15:32:07 +0200

    fixing last merge

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,26 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:speedy_go/presentation/trip_screen/view/pages/map_pages/view/pages/search_map.dart';
-// import 'package:speedy_go/presentation/trip_screen/view/pages/map_pages/view/pages/trip_map_from_to.dart';
+import 'package:speedy_go/presentation/passenger_map_screen/view/pages/search_map.dart';
 import 'package:speedy_go/presentation/buses_screen/pages/add_trip_screen/view/add_trip.dart';
 import 'package:speedy_go/presentation/buses_screen/view/buses_screen.dart';
 import 'package:speedy_go/presentation/main_layout/view/main_layout_view.dart';
 import 'package:speedy_go/presentation/onboarding_screen/view/onboarding_view.dart';
 import 'package:speedy_go/presentation/rating_screen/view/rate_view.dart';
-import 'package:speedy_go/presentation/trip_screen/view/trip_view.dart';
-import 'package:speedy_go/presentation/trip_screen/view/pages/map_pages/view/pages/trip_map_from_to.dart';
 
 import '../../app/sl.dart';
-import '../buses_screen/pages/schedule_screen.dart';
-import '../buses_screen/view/buses_screen.dart';
 import '../buses_screen/pages/schedule_screen/schedule_screen.dart';
 import '../common/transitions/transitions.dart';
 import '../login_screen/view/login_view.dart';
 import '../passenger_map_screen/view/passenger_map_view.dart';
-import '../main_layout/view/main_layout_view.dart';
-import '../onboarding_screen/view/onboarding_view.dart';
 import '../passenger_trip_screen/view/passenger_trip_view.dart';
-import '../rating_screen/view/rate_view.dart';
 import '../register_screen/view/register_view.dart';
 import '../selection_screen/view/selection_view.dart';
 import '../splash_screen/view/splash_view.dart';
@@ -40,7 +32,6 @@ class Routes {
   static const String passengerMapRoute = "/passengerMap";
   static const String passengerTripRoute = "/passengerTrip";
   static const String rateRoute = "/rate";
-  static const String searchMapRoute = "/searchMap";
   static const String busesRoute = "/buses";
   static const String scheduleRoute = "/schedule";
   static const String addTripRoute = "/addTrip";
@@ -76,16 +67,12 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => const PassengerTripScreen());
       case Routes.rateRoute:
         return MaterialPageRoute(builder: (_) => const RateScreen());
-      case Routes.searchMapRoute:
-        return MaterialPageRoute(builder: (_) => const SearchMap());
       case Routes.busesRoute:
         return MaterialPageRoute(builder: (_) => const BusesScreen());
       case Routes.scheduleRoute:
         return MaterialPageRoute(builder: (_) => const ScheduleScreen());
       case Routes.addTripRoute:
         return MaterialPageRoute(builder: (_) => const AddTripScreen());
-      // case Routes.myCustomWidget:
-      //   return MaterialPageRoute(builder: (_) => MyCustomWidget());
       default:
         return unDefinedRoute();
     }
